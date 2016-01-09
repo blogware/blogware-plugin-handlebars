414b5f047bf904e0c8438e59669af9e7ec37ed23
Leo Wong <gnowoel@gmail.com>
2016-01-10 00:55:03 +0800

    Expose a render method for rendering templates

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var type = 'template engine';
 var extnames = ['.hbs'];
 
 var compile = require('./lib/compile');
+var render = require('./lib/render');
 
 exports.type = type;
 exports.extnames = extnames;
 exports.compile = compile;
+exports.render = render;
