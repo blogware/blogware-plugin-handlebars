7fff285436041c7fded814702ace39ea4cc30c6f
Leo Wong <gnowoel@gmail.com>
2016-01-09 22:05:46 +0800

    Reorganize the file structure

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-var handlebars = require('handlebars');
-
 var type = 'template engine';
 var extnames = ['.hbs'];
 
-function compile(contents, cb) {
-  var template;
-
-  try {
-    template = handlebars.compile(contents);
-  } catch(err) {
-    return cb(err);
-  }
-
-  cb(null, template);
-}
+var compile = require('./lib/compile');
 
 exports.type = type;
 exports.extnames = extnames;
