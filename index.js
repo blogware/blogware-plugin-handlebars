68c8e7bcb4e5bbbc1b32fe7ded65e6247c5746f9
Leo Wong <gnowoel@gmail.com>
2016-01-10 00:56:19 +0800

    Expose a route method for mapping a file relative to a URL location

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var type = 'template engine';
 var extnames = ['.hbs'];
-
 var compile = require('./lib/compile');
+var route = require('./lib/route');
 var render = require('./lib/render');
 
 exports.type = type;
 exports.extnames = extnames;
 exports.compile = compile;
+exports.route = route;
 exports.render = render;
