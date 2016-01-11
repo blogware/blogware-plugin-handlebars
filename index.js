7a7e7daf981ec1a32bfbf932e8a85185b4a8394d
Leo Wong <gnowoel@gmail.com>
2016-01-11 21:24:30 +0800

    Expose method for registering a partial

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var type = 'template engine';
 var extnames = ['.hbs'];
 var compile = require('./lib/compile');
+var partial = require('./lib/partial');
 var route = require('./lib/route');
 var render = require('./lib/render');
 
 exports.type = type;
 exports.extnames = extnames;
 exports.compile = compile;
+exports.registerPartial = partial.registerPartial;
 exports.route = route;
 exports.render = render;
