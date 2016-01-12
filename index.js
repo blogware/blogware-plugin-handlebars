df8c4554e34bac7d8c4f220aa0530b63cfcedc1e
Leo Wong <gnowoel@gmail.com>
2016-01-13 02:00:03 +0800

    Rename the `route` method to `rename`

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var type = 'template engine';
 var extnames = ['.hbs'];
 var compile = require('./lib/compile');
 var partial = require('./lib/partial');
-var route = require('./lib/route');
+var rename = require('./lib/rename');
 var render = require('./lib/render');
 
 exports.type = type;
 exports.extnames = extnames;
 exports.compile = compile;
 exports.registerPartial = partial.registerPartial;
-exports.route = route;
+exports.rename = rename;
 exports.render = render;
