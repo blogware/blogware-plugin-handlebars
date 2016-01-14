2c4ec9df6f66d455df399bd9589db518766207a9
Leo Wong <gnowoel@gmail.com>
2016-01-15 00:53:43 +0800

    Register built-in Handlebars partials while initializing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var helper = require('./lib/helper');
+
 var type = 'template engine';
 var extnames = ['.hbs'];
 var compile = require('./lib/compile');
@@ -5,10 +7,25 @@ var partial = require('./lib/partial');
 var rename = require('./lib/rename');
 var render = require('./lib/render');
 
-exports.type = type;
-exports.extnames = extnames;
-exports.compile = compile;
-exports.registerPartial = partial.registerPartial;
-exports.unregisterPartial = partial.unregisterPartial;
-exports.rename = rename;
-exports.render = render;
+var registerPartial = partial.registerPartial;
+var unregisterPartial = partial.unregisterPartial;
+
+function create(opts) {
+  registerHelpers(opts);
+
+  return {
+    type: type,
+    extnames: extnames,
+    compile: compile,
+    registerPartial: registerPartial,
+    unregisterPartial: unregisterPartial,
+    rename: rename,
+    render: render
+  };
+}
+
+function registerHelpers(opts) {
+  helper.register(opts);
+}
+
+exports.create = create;
